9f60fbf146b56e8f89d4fec05df976221fb8cb85
Rmajed001 <ruhulamin5115h@gmail.com>
2025-01-23 20:20:18 +0600

    tuk tukir ma

--- patternprc.c
+++ patternprc.c
@@ -1,24 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int n,row,col;
-printf("Enter N:");
-scanf("%d",&n);
+    int n, row, col;
+    printf("Enter N:");
+    scanf("%d", &n);
 
-for ( row=n;row>=1;row--)
-{
-    for  (col=1;col<=n-row;col++)
+    for (row = n; row >= 1; row--)
     {
-        printf("");
+        for (col = 1; col <= n - row; col++)
+        {
+            printf("");
+        }
+        for (col = 1; col <= row; col++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+
+        5
     }
-    for (col = 1; col <= row; col++)
-    {
-        printf(" * ");
-    }
-    printf("\n");
-    
-    5
+
+    return 0;
 }
 
-return 0;
+// amr sonar bangla ami majed gului mariiiiii...
+
+for (int x = 0; x < 7; x++)
+{
 }
